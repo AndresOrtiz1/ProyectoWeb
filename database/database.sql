4618f4089b12cb3731ac369d013b6890209e6269
msbgod <93262973+msbgod@users.noreply.github.com>
2023-02-06 21:33:40 -0500

    componente de materia prima al 80 %

--- database/database.sql
+++ database/database.sql
@@ -11,3 +11,16 @@ CREATE TABLE `productos` (
   `fecha_actualizacion` datetime NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `materia_prima` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `codigo` varchar(255) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `precio` decimal(10,2) NOT NULL,
+  `unidad_medida` varchar(255) NOT NULL,  
+  `cantidad` decimal(10,2) NOT NULL,
+  `fecha_ingreso` datetime NOT NULL,
+  `fecha_caducidad` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
