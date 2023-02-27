1f1d87eb9fedf1d71073c63e365a35bdb323c926
AndresOrtiz1 <jaortiz16@espe.edu.ec>
2023-02-27 12:34:24 -0500

    commit

--- database/database.sql
+++ database/database.sql
@@ -2,16 +2,7 @@ CREATE DATABASE inventarios_productos_db;
 USE inventarios_productos_db;
 /* La tabla debe cambiar al que le toco esta parte a su conveniencia*/
 
-CREATE TABLE productos (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  nombre varchar(255) NOT NULL,
-  descripcion varchar(255) NOT NULL,
-  precio decimal(10,2) NOT NULL,
-  cantidad int(11) NOT NULL,
-  fecha_creacion datetime NOT NULL,
-  fecha_actualizacion datetime NOT NULL,
-  PRIMARY KEY (id)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
 
 -- tabla de datos de materia prima
 CREATE TABLE materia_prima (
