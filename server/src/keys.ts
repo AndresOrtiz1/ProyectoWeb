0eee0a34910987f6d86514101b8d783bdbee3d40
AndresOrtiz1 <jaortiz16@espe.edu.ec>
2023-02-26 22:02:18 -0500

    clave

--- server/src/keys.ts
+++ server/src/keys.ts
@@ -3,13 +3,9 @@ import Server from "mysql2/typings/mysql/lib/Server"
 export default {
     database: {
         host: 'localhost',
-<<<<<<< HEAD
-        user: 'root', 
-        password: 'andres246', //tu contraseña de tu base de datos clave por defecto "Clave1mysql"
-=======
         user: 'root',   
-        password: 'Clave1mysql', //tu contraseña de tu base de datos clave por defecto "Clave1mysql"
->>>>>>> bfc37cba8ce44f1c42f80e3ef9107aa0e86ca1ab
+        password: 'andres246', //tu contraseña de tu base de datos clave por defecto "Clave1mysql"
+
         database: 'inventarios_productos_db' //tu base de datos
     }
 }
